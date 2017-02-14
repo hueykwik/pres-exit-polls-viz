5e592db3cd9de32309bc7d965e54af3351dbeb73
Huey Kwik <huey.kwik@gmail.com>
2017-02-13 17:42:23 -0800

    Standardize answers that differ slightly between 2012 and 2016, but are actually the same semantically, e.g. Men vs. Male.

--- extract_to_csv.py
+++ extract_to_csv.py
@@ -3,6 +3,32 @@ import json
 from pprint import pprint
 import csv
 
+
+mapping = {"SEX": {"Men": "Male",
+            "Women": "Female"},
+    "RACE": {"African-American": "Black"},
+    "INCOME": {"Less than $50K": "Under $50K",
+                "$50-100K": "$50K-$100K",
+                "$100K or More": "$100K or more"},
+    "UNIONHH12": {"Yes": "Union",
+                  "No": "Not Union"}}
+
+def standardize_row(row):
+    # Some of the same exit poll questions have slightly different answers
+    # from 2012 to 2016, e.g. Men vs. Male. In this function, we fix the
+    # 2012 answer to match the 2016 answer.
+
+    pollname = row[2]
+    answer_name = row[4]
+
+    if pollname in mapping:
+        answer_map = mapping.get(pollname)
+        if answer_name in answer_map:
+            row[4] = answer_map[answer_name]
+
+    return row
+
+
 def extract_file(year, state, csv_writer):
     json_path = "data/%s/%s.json" % (year, state)
     print(json_path)
@@ -27,7 +53,13 @@ def extract_file(year, state, csv_writer):
                     if cand_id == 8639 or cand_id == 893:
                         rep_pct = pct
 
-                csv_writer.writerow([year, state, pollname, question, answer_name, dem_pct, rep_pct])
+                row = [year, state, pollname, question, answer_name, dem_pct, rep_pct]
+
+                row = standardize_row(row)
+
+                csv_writer.writerow(row)
+
+                #csv_writer.writerow([year, state, pollname, question, answer_name, dem_pct, rep_pct])
 
 def extract():
     # Output: CSV year pollname answer state democrat_pct republican_pct
